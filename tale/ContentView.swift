494e1676fd0ef8729d298f2489cac948318b8337
Austin Felder <afellafelder@yahoo.com>
2020-09-14 17:04:56 -0400

    Initial Commit

---
+++ tale/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tale
+//
+//  Created by Austin Felder on 9/14/20.
+//  Copyright © 2020 afellaproductions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
